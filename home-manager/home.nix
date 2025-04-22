849bd06ef421aec0943ea768f7abcaa37aa2d578
misterclayt0n <daviarantes13@gmail.com>
2025-04-22 18:40:37 -0300

    Add a real man's terminal

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,6 +12,7 @@
 		tree
 		jujutsu
 		gh
+		ghostty
 
 		# Nix related.
 		nix-output-monitor # It provides the command `nom` works just like `nix` with more details log output.
@@ -19,23 +20,9 @@
 
 	];
 
-	# TODO: Git.
-	# programs.git = {
-	# 	enable = true;
-	# 	userName = "Ryan Yin";
-	# 	userEmail = "xiaoyin_c@qq.com";
-	# };
-
-	# This value determines the home Manager release that your
-	# configuration is compatible with. This helps avoid breakage
-	# when a new home Manager release introduces backwards
-	# incompatible changes.
-	#
-	# You can update home Manager without changing this value. See
-	# the home Manager release notes for a list of state version
-	# changes in each release.
 	home.stateVersion = "24.11";
 
+	xdg.enable = true;
 	# Let home Manager install and manage itself.
 	programs.home-manager.enable = true;
 
@@ -55,5 +42,6 @@
 	xdg.configFile = {
 	    # This writes ~/.config/jj/config.toml from the jujutsu.toml file.
 	    "jj/config.toml".source = ./jujutsu.toml;
+	    "ghostty/config".text = builtins.readFile ./ghostty.linux;
 	};
 }
