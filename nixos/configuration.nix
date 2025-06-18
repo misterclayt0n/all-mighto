013e6880016e1bf992e875afc4c47826e1c1ee69
misterclayt0n <daviarantes13@gmail.com>
2025-06-17 21:40:18 -0300

    Change default editor to vim

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -135,6 +135,7 @@
     helix
   ];
   environment.variables.EDITOR = "hx";
+  environment.variables.EDITOR = "nvim";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
